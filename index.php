7d0dea906da9d6659482763c437e3f45da133d4b
M Hassan Raza <hassanazmy@gmail.com>
2020-10-18 00:32:21 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,134 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Welcome to Food Recipes Demos</title>
+
+    <!-- Google font-->
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+
+    <!-- Include the font-awesome stylesheet -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Main stylesheet -->
+    <link rel="stylesheet" href="css/main.css?3.2.0"/>
+
+</head>
+    <body class="landing-page">
+
+        <header class="landing-page-header header" role="banner">
+            <div class="logo">
+                <img src="images/logo.png" alt="FoodRecipes">
+                <small class="tag-line">Simply #1 Food & Recipes WordPress Theme</small>
+            </div>
+        </header><!-- .landing-page-header -->
+
+        <main class="landing-page-main main container" role="main">
+
+            <div class="row">
+                <div class="col-sm-6 frame-wrapper">
+                    <div class="item-frame">
+                        <div class="mac-window">
+                            <div class="btns red"></div>
+                            <div class="btns yellow"></div>
+                            <div class="btns green"></div>
+                        </div>
+                        <a href="https://foodrecipes.inspirythemes.com/bootstrap/" target="_blank">
+                            <img src="images/bootstrap.jpg" alt="Food Recipe Bootstrap">
+                        </a>
+                    </div>
+                    <h3 class="">Bootstrap</h3>
+                    <a class="preview-btn" href="https://foodrecipes.inspirythemes.com/bootstrap/" target="_blank"><i class="fa fa-eye"></i><span>Live Preview</span></a>
+                </div>
+
+                <div class="col-sm-6 frame-wrapper">
+                    <div class="item-frame">
+                        <div class="mac-window">
+                            <div class="btns red"></div>
+                            <div class="btns yellow"></div>
+                            <div class="btns green"></div>
+                        </div>
+                        <a href="https://foodrecipes.inspirythemes.com/classic/" target="_blank">
+	                        <img src="images/classic.jpg" alt="Food Recipe Classic">
+                        </a>
+                    </div>
+                    <h3 class="">Classic</h3>
+                    <a class="preview-btn" href="https://foodrecipes.inspirythemes.com/classic/" target="_blank"><i class="fa fa-eye"></i><span>Live Preview</span></a>
+                </div>
+            </div>
+	        <div class="v-space">
+		        <hr>
+	        </div>
+
+			<!-- Color Variations -->
+	        <div class="row">
+		        <h3 class="landing-title">Some Color Variations</h3>
+		        <div class="col-sm-3 frame-wrapper">
+			        <div class="item-frame">
+				        <div class="mac-window">
+					        <div class="btns red"></div>
+					        <div class="btns yellow"></div>
+					        <div class="btns green"></div>
+				        </div>
+				        <a href="https://foodrecipes.inspirythemes.com/bootstrap/?skin=skin_default" target="_blank">
+					        <img src="images/fr-skin-default-green.png" alt="Food Recipe Default Green Skin">
+				        </a>
+			        </div>
+			        <h3 class="">Default Green</h3>
+			        <a class="preview-btn" href="https://foodrecipes.inspirythemes.com/bootstrap/?skin=skin_default" target="_blank"><i class="fa fa-eye"></i><span>Live Preview</span></a>
+		        </div>
+
+		        <div class="col-sm-3 frame-wrapper">
+			        <div class="item-frame">
+				        <div class="mac-window">
+					        <div class="btns red"></div>
+					        <div class="btns yellow"></div>
+					        <div class="btns green"></div>
+				        </div>
+				        <a href="https://foodrecipes.inspirythemes.com/bootstrap/?skin=skin_blue" target="_blank">
+					        <img src="images/fr-skin-blue.png" alt="Food Recipe Blue Skin">
+				        </a>
+			        </div>
+			        <h3 class="">Blue</h3>
+			        <a class="preview-btn" href="https://foodrecipes.inspirythemes.com/bootstrap/?skin=skin_blue" target="_blank"><i class="fa fa-eye"></i><span>Live Preview</span></a>
+		        </div>
+
+		        <div class="col-sm-3 frame-wrapper">
+			        <div class="item-frame">
+				        <div class="mac-window">
+					        <div class="btns red"></div>
+					        <div class="btns yellow"></div>
+					        <div class="btns green"></div>
+				        </div>
+				        <a href="https://foodrecipes.inspirythemes.com/bootstrap/?skin=skin_red" target="_blank">
+					        <img src="images/fr-skin-red.png" alt="Food Recipe Red Skin">
+				        </a>
+			        </div>
+			        <h3 class="">Red</h3>
+			        <a class="preview-btn" href="https://foodrecipes.inspirythemes.com/bootstrap/?skin=skin_red" target="_blank"><i class="fa fa-eye"></i><span>Live Preview</span></a>
+		        </div>
+
+		        <div class="col-sm-3 frame-wrapper">
+			        <div class="item-frame">
+				        <div class="mac-window">
+					        <div class="btns red"></div>
+					        <div class="btns yellow"></div>
+					        <div class="btns green"></div>
+				        </div>
+				        <a href="https://foodrecipes.inspirythemes.com/bootstrap/?skin=skin_orange" target="_blank">
+					        <img src="images/fr-skin-orange.png" alt="Food Recipe Orange Skin">
+				        </a>
+			        </div>
+			        <h3 class="">Orange</h3>
+			        <a class="preview-btn" href="https://foodrecipes.inspirythemes.com/bootstrap/?skin=skin_orange" target="_blank"><i class="fa fa-eye"></i><span>Live Preview</span></a>
+		        </div>
+	        </div>
+        </main><!-- .landing-page-main -->
+       
+
+    </body>
+</html>
